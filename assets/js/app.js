6218c214b94de0e49a6600fd85e3ae4e26e4d11d
EmilyT <emilython@hotmail.com>
2021-09-26 17:00:25 -0500

    added save to local storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,7 +15,6 @@ function setArray(){
     $timeBlocks.each(function(){
         var $thisBlock = $(this);
         var thisHour = parseInt($thisBlock.attr("data-hour"));
-
         var taskObject = {
             hour: thisHour,
             text: "",
@@ -27,7 +26,6 @@ function setArray(){
     // save array to local storage
     localStorage.setItem("tasks", JSON.stringify(taskArr));
     console.log(taskArr);
-
 }
 
 // set timeblock colors based on past, present, future
@@ -48,3 +46,48 @@ function setBlockColor(){
         }
     });
 }
+
+// display tasks on schedule
+function displaySchedule() {
+    taskArr = localStorage.getItem("tasks");
+    taskArr = JSON.parse(taskArr)
+
+    // assign text to timeblock
+    for (var i = 0; i < taskArr.length; i++) {
+        var taskHour = taskArr[i].hour;
+        var taskText = taskArr[i].text;
+
+        $("[data-hour=" + taskHour + "]").children("textarea").val(taskText);
+    }
+    console.log(taskArr);
+}
+
+// save schedule to local storage
+function saveSchedule(){
+
+    var updateHour = $(this).parent().attr("data-hour");
+    var addTask = (($(this).parent()).children("textarea")).val();
+
+    // loop through  tasks to see if it needs updating based on hour
+    for (var i = 0; i < taskArr.length; i++) {
+        if(taskArr[i].hour == updateHour){
+            taskArr[i].text = addTask;
+        }
+    }
+    localStorage.setItem("tasks", JSON.stringify(taskArr));
+    displaySchedule();
+}
+
+// load schedule
+$(document).ready(function(){
+    setBlockColor();
+    if(!localStorage.getItem("tasks")){
+        setArray();
+    }
+    // show current day
+    $currentDay.text(currentDate);
+    // render schedule
+    displaySchedule();
+    // save schedule on click
+    $schedule.on("click", "button", saveSchedule);
+});
